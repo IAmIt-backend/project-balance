57768c2dc86592e693bf88dda84e4a8fec9f958e
egorix2000 <egorix2000@mail.ru>
2016-03-28 13:31:45 +0300

    add models and entities

---
+++ Balance/Balance/Models/CreditModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Balance.Models
+{
+    public class CreditModel
+    {
+        public int Money { get; set; }
+        //public int DebtorId { get; set; }
+        //public int CreditorId { get; set; }
+    }
+}
