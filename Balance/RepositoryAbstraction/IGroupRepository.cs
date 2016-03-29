513aff29b3d44d12d8364ea09b62c9b1a4f2d684
Dumpling545 <Yanik_K_2000@mail.ru>
2016-03-29 12:39:34 +0300

    Add IGroupRepository

---
+++ Balance/RepositoryAbstraction/IGroupRepository.cs
@@ -0,0 +1,17 @@
+﻿using Balance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryAbstraction
+{
+    public interface IGroupRepository
+    {
+        Task<bool> AddGroup(GroupModel groupModel);
+        Task<bool> AddUserToGroup(string userEmail, string GroupName, string GroupDescription);
+        Task<GroupModel> GetGroup(string GroupName, string GroupDescription);
+        Task<ICollection<GroupModel>> GetAllGroups();
+    }
+}
