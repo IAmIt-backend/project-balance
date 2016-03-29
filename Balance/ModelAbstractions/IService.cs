ffd1fb87cd6a63d5e251a3f694753c77d765104b
ottoRoBaGO <ottorobago@bk.ru>
2016-03-29 12:17:15 +0200

    Added ModelAbstractions

---
+++ Balance/ModelAbstractions/IService.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelAbstractions
+{
+    public interface IService
+    {
+       void AddGroup(ObjectId groupId);
+       void AddUserToGroup(ObjectId userId, ObjectId groupId);
+       void GetAllGroups();
+    }
+}
