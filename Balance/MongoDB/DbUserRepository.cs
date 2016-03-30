2ba9325e0dfdad846f13af7a282bb13697366f9d
Dumpling545 <Yanik_K_2000@mail.ru>
2016-03-30 16:54:34 +0300

    add methods in repository realizations

---
+++ Balance/MongoDB/DbUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using RepositoryAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Entities;
+
+namespace MongoDB
+{
+    class DbUserRepository : IUserRepository
+    {
+        private IMongoCollection<Group> _groups;
+        private IMongoCollection<UserGroupMembership> _memberships;
+        public DbUserRepository(){
+            var client = new MongoClient();
+            var db = client.GetDatabase("balance");
+            _groups = db.GetCollection<Group>(nameof(Group));
+            _memberships = db.GetCollection<UserGroupMembership>(nameof(UserGroupMembership));
+        }
+        public async Task<ICollection<ObjectId>> GetAllGroupsOfUser(ObjectId userId)
+        {
+            return (await _memberships.FindAsync(m => m.UserId == userId)).ToList().Select(m => m.GroupId).ToList();
+        }
+    }
+}
